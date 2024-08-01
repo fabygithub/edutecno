1063ff5959dca35eccc7eb0359df5f7a892a9f67
fabian cornejo <36684138+fabygithub@users.noreply.github.com>
2024-08-01 11:03:21 -0400

    Add files via upload

---
+++ evaluacion_m10/assets/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+    $(".text-body-secondary").click(function() {  
+    var idBoton = $(this).attr("id");
+      $("#detalles" + idBoton).toggle();
+    });
+
+
+    $(".btn-close").click(function() {
+        $(".detalles").hide();
+    });
+  });
+
+
+   
+
+   
